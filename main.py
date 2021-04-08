caf60810914f92a496af882c0b57da4204db85c0
varCODEx <borys.kovba@gmail.com>
2021-04-08 14:00:14 +0300

    runge method for accuracy

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ true_result = -0.006294843240543572
 
 x0 = -1
 xk = 1
-h1 = 0.5
-h2 = 0.25
+h = 0.5
+h_2 = 0.25
 
 
 def rectangles_method(f, a, b, dx=0.1):
@@ -50,20 +50,29 @@ def simpson_method(f, a, b, dx=0.1):
 print(f'True result is {true_result}')
 print()
 
-r1 = rectangles_method(f, x0, xk, h1)
-r2 = rectangles_method(f, x0, xk, h2)
-print(f'rectangles method for step {h1} yields: {r1} ({(r1 - true_result) * 100 / true_result:.2f}% error)')
-print(f'rectangles method for step {h2} yields: {r2} ({(r2 - true_result) * 100 / true_result:.2f}% error)')
+r1 = rectangles_method(f, x0, xk, h)
+r2 = rectangles_method(f, x0, xk, h_2)
+print(f'rectangles method for step {h} yields: {r1} ({(r1 - true_result) * 100 / true_result:.2f}% error)')
+print(f'rectangles method for step {h_2} yields: {r2} ({(r2 - true_result) * 100 / true_result:.2f}% error)')
+p = 1
+r12 = r2 + (r2-r1)/(2**p-1)
+print(f'rectangles*runge method yields: {r12} ({(r12 - true_result) * 100 / true_result:.2f}% error)')
 print()
 
-t1 = trapezoid_method(f, x0, xk, h1)
-t2 = trapezoid_method(f, x0, xk, h2)
-print(f'trapezoid method for step {h1} yields: {t1} ({(t1 - true_result) * 100 / true_result:.2f}% error)')
-print(f'trapezoid method for step {h2} yields: {t2} ({(t2 - true_result) * 100 / true_result:.2f}% error)')
+t1 = trapezoid_method(f, x0, xk, h)
+t2 = trapezoid_method(f, x0, xk, h_2)
+print(f'trapezoid method for step {h} yields: {t1} ({(t1 - true_result) * 100 / true_result:.2f}% error)')
+print(f'trapezoid method for step {h_2} yields: {t2} ({(t2 - true_result) * 100 / true_result:.2f}% error)')
+p = 2
+t12 = t2 + (t2-t1)/(2**p-1)
+print(f'trapezoid* method yields: {t12} ({(t12 - true_result) * 100 / true_result:.2f}% error)')
 print()
 
-s1 = simpson_method(f, x0, xk, h1)
-s2 = simpson_method(f, x0, xk, h2)
+s1 = simpson_method(f, x0, xk, h)
+s2 = simpson_method(f, x0, xk, h_2)
+p = 4
+s12 = s2 + (s2-s1)/(2**p-1)
+print(f'simpson method for step {h} yields: {s1} ({(s1 - true_result) * 100 / true_result:.2f}% error)')
+print(f'simpson method for step {h_2} yields: {s2} ({(s2 - true_result) * 100 / true_result:.2f}% error)')
 
-print(f'simpson method for step {h1} yields: {s1} ({(s1 - true_result) * 100 / true_result:.2f}% error)')
-print(f'simpson method for step {h2} yields: {s2} ({(s2 - true_result) * 100 / true_result:.2f}% error)')
+print(f'simpson* method yields: {s12} ({(s12 - true_result) * 100 / true_result:.2f}% error)')
